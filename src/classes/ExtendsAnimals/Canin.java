bdb6c01559407a8ebc054b0412ac8a0cec5bab06
Michelle <ngamimarrion@gmail.com>
2021-06-11 16:33:00 +0100

    Ajout du fichier Canin.java qui contient la classe abstraite Canin qui extends Animal

---
+++ src/classes/ExtendsAnimals/Canin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.ExtendsAnimals;
+
+import classes.Abstract.Animal;
+
+/**
+ *
+ * @author ngami_4vnxadk
+ */
+public abstract class Canin extends Animal {
+    void Walker()
+    {
+        System.out.print(" je me deplace en meute");
+                    
+    }
+    
+}
