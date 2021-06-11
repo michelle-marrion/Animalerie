70eb489c40289feb8cee8151a622bd323ff6042e
Michelle <ngamimarrion@gmail.com>
2021-06-11 16:38:18 +0100

    Ajout du fichier Felin.java qui contient la classe abstraite Felin qui extends la classe Animal

---
+++ src/classes/ExtendsAnimals/Felin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.ExtendsAnimals;
+
+import classes.Abstract.Animal;
+
+/**
+ *
+ * @author ngami_4vnxadk
+ */
+public abstract class Felin extends Animal {
+    void Walker()
+    {
+        System.out.println("je me déplace seul");
+    }
+}
