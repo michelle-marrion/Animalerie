fb4164742a3e5108eb47806c5570100e05cb557c
Michelle <ngamimarrion@gmail.com>
2021-06-11 15:08:09 +0100

    Cration de la classse abstraite Animal

---
+++ src/classes/Abstract/Animal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes.Abstract;
+
+/**
+ *
+ * @author ngami_4vnxadk
+ */
+public abstract class  Animal {
+     protected int poid;
+    protected String couleur;
+    
+    // constructeur par défaut 
+    //methodes
+protected void Eat()
+{
+    System.out.println("I eat meat !");
+}
+protected void Drink() 
+{
+    System.out.println("I drink Water !");
+}
+ abstract void Walker();// un animal peux marcher, ramper ou voler
+ abstract  void Cry(); // un animal à un crie
+ 
+ //decription de la methode
+ 
+ public String toString()
+ {
+     return "je suis un Objet de la Classe"+this.getClass()+"je suis de Couleur "+this.couleur+" et je pèse "+this.poid+" kg";
+ }
+}
